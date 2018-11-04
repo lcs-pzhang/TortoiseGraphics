98e193cdf9fe52d2d865fc7aa26ed4b76c2768bb
Phillip Zhang <pzhang@lcs.on.ca>
2018-11-03 22:00:04 -0400

    finished as much as i could

--- Playground/Playground.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
+++ Playground/Playground.playground/Pages/Assignment.xcplaygroundpage/Contents.swift
@@ -12,98 +12,126 @@ PlaygroundPage.current.liveView = canvas
 
 canvas.drawing { t in
     // Start name in upper left corner
-    t.penUp()
-    t.goto(-350, 260)
-
-    t.uppercaseP()
-    t.lowercaseH()
-    t.firsti()
-    t.firstl()
-    t.secondl()
-    t.secondi()
-    t.secondp()
-    t.capitalZ()
-    t.secondlowerh()
-    t.lowera()
-    t.lowern()
-    t.lowerg()
+//    t.penUp()
+//    t.goto(-350, 260)
+//
+//    t.uppercaseP()
+//    t.lowercaseH()
+//    t.firsti()
+//    t.firstl()
+//    t.secondl()
+//    t.secondi()
+//    t.secondp()
+//    t.capitalZ()
+//    t.secondlowerh()
+//    t.lowera()
+//    t.lowern()
+//    t.lowerg()
     
-    
-    
-    
-    
-   
-
-    //Capital Z
+    //zhang
     t.penUp()
     t.left(150)
     t.forward(200)
-    t.right(150)
-    t.backward(40)
-    t.penDown()
-    t.curve(withSides: 25, withSize: 7, drawSides: 13)
-    t.left(45)
-    t.curve(withSides: 80, withSize: 7, drawSides: 7)
     t.left(120)
-    t.curve(withSides: 30, withSize: 7, drawSides: 17)
-    t.right(60)
-    t.curve(withSides: 50, withSize: 11, drawSides: 9)
-    t.ycor
-    t.xcor
-    t.heading
-
-    //h
-    t.left(15)
-    t.curve(withSides: -20, withSize: 16, drawSides: 7)
-    t.curve(withSides: -20, withSize: 2, drawSides: 7)
+    t.backward(200)
     t.penDown()
+    t.forward(80)
+    t.left(180)
+    t.left(90)
+    t.forward(40)
+    t.right(90)
+    t.forward(80)
+    t.left(90)
+    t.forward(30)
+    t.left(90)
+    t.forward(80)
+    t.left(270)
+    t.forward(50)
+    t.right(105)
+    t.forward(50)
+    t.penDown()
+    t.penUp()
+    t.left(255)
     t.forward(130)
+    t.left(210)
+    t.penDown()
+    t.forward(120)
+    t.left(180)
+    t.forward(60)
+    t.right(90)
+    t.forward(50)
+    t.left(180)
+    t.forward(50)
+    t.left(105)
+    t.curve(withSides: -65, withSize: 6, drawSides: 13)
+    t.penUp()
+    t.left(90)
+    t.forward(45)
+    t.right(90)
+    t.backward(60)
+    t.left(20)
+    t.curve(withSides: -70, withSize: 6, drawSides: 13)
+    
+    //tian
+    t.left(8)
+    t.right(90)
+    t.forward(70)
+    t.penDown()
+    t.forward(80)
+    t.left(180)
+    t.forward(40)
+    t.left(90)
+    t.curve(withSides: -75, withSize: 7, drawSides: 15)
+    t.left(25)
+    t.penUp()
+    t.backward(80)
+    t.left(180)
+    t.right(145)
+    t.curve(withSides: -75, withSize: 7, drawSides: 15)
+    t.left(30)
+    t.penUp()
+    t.backward(50)
+    t.left(35)
+    t.penDown()
+    t.forward(70)
+    
+    //xing
+    t.penUp()
+    t.backward(250)
+    t.penDown()
+    t.left(45)
+    t.forward(40)
+    t.left(45)
+    t.forward(50)
     t.left(180)
     t.penUp()
-    t.forward(30)
+    t.forward(60)
+    t.right(45)
     t.penDown()
-    t.curve(withSides: 30, withSize: 5, drawSides: 19)
-    t.curve(withSides: -10, withSize: 6, drawSides: 7)
-
-    //a
-    t.right(30)
-    t.curve(withSides: 31, withSize: 5, drawSides: 31)
-    t.right(90)
-    t.penUp()
+    t.forward(60)
+    t.left(180)
     t.forward(50)
-    t.right(90)
-    t.penDown()
-    t.curve(withSides: -20, withSize: 5, drawSides: 7)
-
-    //n
-    t.left(45)
-    t.curve(withSides: 100, withSize: 3, drawSides: 15)
-    t.penDown()
-    t.right(120)
-    t.forward(40)
+    t.left(100)
     t.penUp()
-    t.backward(39)
-    t.left(150)
-    t.curve(withSides: 30, withSize: 5, drawSides: 12)
-    t.right(10)
-    t.curve(withSides: -20, withSize: 5, drawSides: 7)
-
-    //g
-    t.left(70)
-    t.curve(withSides: 11, withSize: 10, drawSides: 11)
-    t.right(110)
-    t.forward(29)
-    t.left(205)
-    t.forward(10)
-    t.penDown()
-    t.forward(90)
-    t.curve(withSides: 25, withSize: 7, drawSides: 17)
-    t.left(360)
-    t.penDown()
     t.forward(100)
-//
-//
-//
+    t.penDown()
+    t.forward(25)
+    t.left(125)
+    t.forward(85)
+    t.left(90)
+    t.forward(35)
+    t.backward(70)
+    t.right(90)
+    t.penUp()
+    t.forward(20)
+    t.penDown()
+    t.left(90)
+    t.forward(80)
+
+
+    
+    
+    
     
     
 }
